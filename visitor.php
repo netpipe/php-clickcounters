3f2caeaea004f348abfb58b6737fda692937a4d4
netpipe <teslaenergy@gmail.com>
2022-11-28 17:13:34 -0700

    testing

--- visitor.php
+++ visitor.php
@@ -18,7 +18,31 @@ $content = file_get_contents('visitors.txt');
 echo $content . "testing";
 
 
-session_unset();
+
+//https://stackoverflow.com/questions/3003145/how-to-get-the-client-ip-address-in-php
+// Function to get the user IP address
+function getUserIP() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+echo "<br> ip : " . getUserIP();
+
 ?>
   
 
