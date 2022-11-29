ea98fd0d714c9f61e296b3b52fad0473c46a733e
netpipe <teslaenergy@gmail.com>
2022-11-28 17:24:17 -0700

    testing

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+   <meta http-equiv="refresh" content="500">
+<body>
+
+  <h1>Order Page</h1>
+
+<p>Check out <a href="./kills.php">kills</a>.</p>
+<p>Check out <a href="./bh.php">3-5 ton blackhole</a>.</p>
+<p>Check out <a href="./co2.php">1 ton of co2</a>.</p>
+<p>Check out <a href="./visitor.php">visitor</a>.</p>
+<br>
+for educational and speritual use<br>
+</body>
+</html>
